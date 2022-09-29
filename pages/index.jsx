d964f8841899ce3323ca7e42db1dbbb9d25d7917
MiguelMatos24 <miguelmatoss24@gmail.com>
2022-09-29 16:45:24 +0100

    new

--- pages/index.jsx
+++ pages/index.jsx
@@ -2,10 +2,8 @@ import Layout from "../components/layout";
 import Form from "../components/form";
 export default function Home() {
   return (
-    <Layout>
-      <div className="container py-5">
-        <Form />
-      </div>
+    <Layout className="mt-5">
+      <Form />
     </Layout>
   );
 }
