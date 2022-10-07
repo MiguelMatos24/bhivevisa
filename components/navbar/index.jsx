a1efd092ee94d15da2c4b56faf221ab4e92c18f3
MiguelMatos24 <miguelmatoss24@gmail.com>
2022-10-07 17:24:49 +0100

    bug nav

--- components/navbar/index.jsx
+++ components/navbar/index.jsx
@@ -3,46 +3,6 @@ import Image from "next/image";
 import { slide as Menu } from "react-burger-menu";
 
 const nav = () => {
-  const menuItems = [
-    {
-      title: "About us",
-      url: "/about",
-    },
-    {
-      title: "Global Mobility & Immigration",
-      url: "/global-mobility",
-    },
-    {
-      title: "Tax Representation",
-      url: "/tax",
-    },
-    {
-      title: "Property Management",
-      url: "/property",
-    },
-    {
-      title: "Contact us",
-      url: "/contacts",
-    },
-  ];
-  const subMenu = [
-    {
-      title: "Golden Visa",
-      url: "#",
-    },
-    {
-      title: "Digital Nomad Visa",
-      url: "#",
-    },
-    {
-      title: "Citizenship",
-      url: "#",
-    },
-    {
-      title: "Refugee & Asylum",
-      url: "#",
-    },
-  ];
   return (
     <nav className="nav-container">
       <div classname="container-fluid">
@@ -60,55 +20,97 @@ const nav = () => {
           </div>
           <div className="menu-container">
             <Menu right>
-              {menuItems.map((menu, key) => {
-                return (
-                  <Link className="menu-item" href={menu.url} key={key}>
-                    {menu.title}
-                  </Link>
-                );
-              })}
+              <Link className="menu-item" href="/about">
+                About us
+              </Link>
+              <Link href="/global-mobility">
+                <a
+                  className="menu-item dropdown-toggle nav-link"
+                  id="navbarDarkDropdownMenuLink"
+                  role="button"
+                  data-bs-toggle="dropdown"
+                  aria-expanded="false"
+                >
+                  Global Mobility & Immigration
+                </a>
+              </Link>
+              <ul
+                className="dropdown-menu"
+                aria-labelledby="navbarDarkDropdownMenuLink"
+              >
+                <Link href="#">
+                  <a className="dropdown-item">Action</a>
+                </Link>
+                <Link href="#">
+                  <a className="dropdown-item">Action</a>
+                </Link>
+                <Link href="#">
+                  <a className="dropdown-item">Action</a>
+                </Link>
+              </ul>
+
+              <Link className="menu-item" href="/tax">
+                Tax Representation
+              </Link>
+              <Link className="menu-item" href="/property">
+                Property Management
+              </Link>
+              <Link className="menu-item" href="/contact">
+                Contact us
+              </Link>
             </Menu>
           </div>
         </div>
         <div className="row-nav">
-          {menuItems.map((menu, key) => {
-            return (
-              <Link href={menu.url} key={key}>
-                {menu.title}
-              </Link>
-            );
-          })}
-
-          <a
-            className="nav-link dropdown-toggle"
-            href="#"
-            id="navbarDarkDropdownMenuLink"
-            role="button"
-            data-bs-toggle="dropdown"
-            aria-expanded="false"
-          >
-            Dropdown
-          </a>
+          <Link className="menu-item" href="/about">
+            About us
+          </Link>
+          <Link className="menu-item" href="/global-mobility">
+            <a
+              href="#"
+              className="dropdown-toggle nav-link"
+              id="navbarDarkDropdownMenuLink"
+              role="button"
+              data-bs-toggle="dropdown"
+              aria-expanded="false"
+            >
+              Global Mobility & Immigration
+            </a>
+          </Link>
           <ul
-            className="dropdown-menu dropdown-menu-dark"
+            className="dropdown-menu "
             aria-labelledby="navbarDarkDropdownMenuLink"
           >
             <li>
               <a className="dropdown-item" href="#">
-                Action
+                Golden Visa
               </a>
             </li>
             <li>
               <a className="dropdown-item" href="#">
-                Another action
+                Digital Nomad Visa
               </a>
             </li>
             <li>
               <a className="dropdown-item" href="#">
-                Something else here
+                Citizenship
+              </a>
+            </li>
+            <li>
+              <a className="dropdown-item" href="#">
+                Refugee & Asylum
               </a>
             </li>
           </ul>
+          <Link className="menu-item" href="/tax">
+            Tax Representation
+          </Link>
+          <Link className="menu-item" href="/property">
+            Property Management
+          </Link>
+          <Link className="menu-item" href="/contact">
+            Contact us
+          </Link>
         </div>
       </div>
     </nav>
